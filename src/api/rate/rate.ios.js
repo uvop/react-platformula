248a8102e1b6ec05ccd468fb0a390763654d0229
Omer Prizner <omer.prizner@gmail.com>
2018-01-13 19:20:01 +0200

    removed rate component until it's API is fully finished

--- src/api/rate/rate.ios.js
+++
@@ -1,8 +0,0 @@
-import * as StoreReview from 'react-native-store-review';
-
-export default () => {
-  // This API is only available on iOS 10.3 or later
-  if (StoreReview.isAvailable) {
-    StoreReview.requestReview();
-  }
-};
--- src/module/rate.js
+++
@@ -1,4 +0,0 @@
-import Rate, { ANDROID_PACKAGE_STORE } from 'src/api/rate';
-
-// eslint-disable-next-line no-undef
-module.exports = Object.assign(Rate, { ANDROID_PACKAGE_STORE });
